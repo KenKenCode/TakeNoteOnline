e65789c5fee4701c37f17255c3598b73682f5144
KenKenCode <kennethlobingcofabic@outlook.com>
2022-12-15 08:35:14 +0800

    New

--- LogInPage.php
+++ LogInPage.php
@@ -5,14 +5,29 @@ session_start();
 $conn=mysqli_connect("localhost", "root", "", "tnstudentregistrationdb");
 
 if(isset($_POST['submit'])) {
+
+    if(empty(trim['userNameName']) && empty(trim($_POST['passwordName']))) {
+        echo '<script type="text/javascript"> alert("Input fields must have values (email, password)"); </script>';
+    } else {
     //set variables for sql query
     $usernameSignIn = mysqli_real_escape_string($conn, $_POST['userNameID']);
     $passwordSignIn = mysqli_real_escape_string($conn, $_POST['passwordID']);
 
-    //scan for the set values in the variables for log-in
-    $sqlLogIn = mysqli_query($conn, "SELECT * FROM ")
+    $res = $conn->query("SELECT student_password FROM registrationforlogin WHERE student_name = '$usernameSignIn'");
+    
+    $hashed_pass = $res->fetch_assoc()['student_password'];
+
+    if(!password_verify($passwordSignIn, $hashed_pass)) {
+
+        $login_error = "Wrong password. Please try again";
+
+    } else {
+        $login_success = "Login Successful";
+        //header('Location:notebookspage2.php');
+    }
 
 }
+} 
 ?>
 
 <!DOCTYPE html>
@@ -34,10 +49,10 @@ if(isset($_POST['submit'])) {
             <form action="" method="POST">
                 
                 <label for="emailID">Email: </label><br /> <!--label for will bind to input element's id-->
-                <input type="text" id="userNameID" name="emailName" placeholder="Type your username" class = "logInTextbox"> <br />
+                <input type="text" id="userNameID" name="userNameName" placeholder="Type your username" class = "logInTextbox"> <br />
 
                 <label for="passwordID">Password: </label><br /> <!--label for will bind to input element's id-->
-                <input type="password" id="passwordID" name="emailName" placeholder="Type your password" class = "logInTextbox"> <br />
+                <input type="password" id="passwordID" name="passwordName" placeholder="Type your password" class = "logInTextbox"> <br />
 
                 <input type="submit" value="Log-In" class="logInButton">                      
             </form>
